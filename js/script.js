d33b14d02dfa7c04cb3385c58785d7c8f40f8f54
Rost <example.com>
2021-04-24 17:56:44 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use stricr";
+
+/* let number  = 4.6;
+
+console.log(4/0);
+console.log('string'*9);
+
+const persone = 'Alex'; 
+
+const bool = true;  
+
+const obj = {
+     name: "Jonn",
+     age: 25,
+     isMarried: false
+};
+
+console.log(obj.name);
+console.log(obj["name"]); 
+
+let arr = ['plump.png', 'onratechange.jpg', 'apple.bmp'];
+console.log(arr[1]); */
+
+/* alert('Hello'); */
+
+/* const result = confirm("Are you here?");
+console.log(result); */ 
+
+/* const answer = prompt("WTF??", "18");
+console.log(answer + 5); */
+
+const answers = [];
+
+ answers[0] = prompt('Как ваше имя?', '');
+ answers[1] = prompt('Как ваша фамилия?', '');
+ answers[2] = prompt('Тебе сколько лет?', '');
+
+ document.write(answers);
+
